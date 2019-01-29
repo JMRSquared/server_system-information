a77a3164f9463dc205eed0a409419169fee46deb
Sirwali Joseph <mulavhe@gmail.com>
2019-01-29 20:26:04 +0200

    Added origin checks

--- server/app.js
+++ server/app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
@@ -36,7 +36,7 @@ app.use(function(req, res, next) {
 });
 
 mongoose
-    .connect("mongodb://68.66.193.181:27017/JMRSquaredDB?authSource=admin", {
+    .connect("mongodb://localhost:27017/JMRSquaredDB?authSource=admin", {
         auth: {
             user: "admin",
             password: "Mulavhelesi@1"
@@ -56,7 +56,7 @@ app.use("/stats", statsController);
 app.use("/ovalleaf", ovalleafController);
 
 /// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error("Not Found");
     err.status = 404;
     next(err);
@@ -67,7 +67,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get("env") === "development") {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render("error", {
             message: err.message,
@@ -78,7 +78,7 @@ if (app.get("env") === "development") {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render("error", {
         message: err.message,
